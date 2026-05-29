4976ac47a3db40e90f914f867ff8f573351538fb
shilpijaiswal02 <shilpijais9693@gmail.com>
2026-05-29 16:49:47 +0530

    Time: 129 ms (22.93%), Space: 46.9 MB (68.88%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -4,16 +4,16 @@ class Solution {
        Map<Integer,Integer> map=new HashMap<>();
        int sum=0;
        int count=0;
-         for(int i : nums1){
-            for(int j : nums2){
-            sum=i+j; 
+         for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+            sum=nums1[i]+ nums2[j]; 
             map.put(sum, map.getOrDefault(sum,0)+1);
          }
         }
          int sum2=0;
-         for(int k:nums3){
-            for(int l:nums4){
-                sum2=k+l; 
+         for(int k=0;k<nums3.length;k++){
+            for(int l=0;l<nums4.length;l++){
+                sum2=nums3[k]+ nums4[l]; 
 
                 if(map.containsKey(-sum2)){
                     count+=map.get(-sum2);
