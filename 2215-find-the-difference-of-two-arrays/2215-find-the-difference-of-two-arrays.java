185df7dc184e783c1997e759d98ec20118dbb8d3
shilpijaiswal02 <shilpijais9693@gmail.com>
2026-06-11 22:06:01 +0530

    Time: 11 ms (42.44%), Space: 47.4 MB (73.02%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -4,6 +4,29 @@ class Solution {
         Set<Integer> list1= new HashSet<>();
         Set<Integer> list2= new HashSet<>();
 
+        List<Integer> list3= new ArrayList<>();
+        List<Integer> list4= new ArrayList<>();
+
+        for(int i=0;i<nums1.length;i++){
+            list1.add(nums1[i]);
+        }
+         for(int i=0;i<nums2.length;i++){
+            list2.add(nums2[i]);
+        }
+        
+        for(int num:list1){
+            if(!list2.contains(num)){
+                list3.add(num); 
+            }
+        }
+          list.add(new ArrayList<>(list3));
+        for(int num:list2){
+            if(!list1.contains(num)){
+                list4.add(num); 
+            }
+        }
+          list.add(new ArrayList<>(list4));
+/*
         for(int i=0;i<nums1.length;i++){
               boolean flag= true;
            
@@ -31,7 +54,8 @@ class Solution {
             }
         }
          list.add(new ArrayList<>(list2));
-
+*/
+    
         return list;
     }
 }
