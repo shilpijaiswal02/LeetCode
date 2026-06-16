219226c364c62bde08eb5fb870de7cabce3fd22e
shilpijaiswal02 <shilpijais9693@gmail.com>
2026-06-16 21:42:33 +0530

    Time: 3 ms (100%), Space: 55.8 MB (12.5%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String processStr(String s) {
+       StringBuilder sb = new StringBuilder();
+       for(int i=0;i<s.length();i++){
+        if(Character.isLetter(s.charAt(i))){
+            sb.append(s.charAt(i));
+        }
+        else if(s.charAt(i)=='#'){
+            sb.append(sb.toString());
+        }
+        else if(s.charAt(i)=='%'){
+            sb.reverse();
+        }else if(s.charAt(i)=='*'){
+            if(sb.length()>0){
+            sb.deleteCharAt(sb.length()-1);
+            }
+        }
+
+       }
+
+       return sb.toString();
+    }
+}
