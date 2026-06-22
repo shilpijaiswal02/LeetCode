66c6c6456916209099706b4350a0271b47a850ce
shilpijaiswal02 <shilpijais9693@gmail.com>
2026-06-22 15:48:38 +0530

    Time: 1 ms (29.77%), Space: 43.2 MB (12.36%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        StringBuilder sb= new StringBuilder(s);
+          
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(!Character.isLetter(sb.charAt(i))){
+                
+                i++;
+               continue;
+                
+            }
+            if(!Character.isLetter(sb.charAt(j))){
+                j--;
+                continue;
+            }
+            if(Character.isLetter(sb.charAt(i))&& Character.isLetter(sb.charAt(j))){
+               char temp= sb.charAt(i);
+               sb.setCharAt(i, sb.charAt(j));
+               sb.setCharAt(j, temp);
+               i++;
+               j--;
+            }
+
+        }
+        return sb.toString();
+    }
+}
