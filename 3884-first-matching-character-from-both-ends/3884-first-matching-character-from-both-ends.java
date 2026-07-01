d5d8339681b5fe61ae4e4a0bc52e62bbd5631228
Shilpi Kumari <shilpijais9693@gmail.com>
2026-07-01 23:33:20 +0530

    Time: 1 ms (38.26%), Space: 43.1 MB (84.06%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+       
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)==s.charAt(j)){
+                return Math.min(i,j);
+
+            }
+            i++;
+            j--;
+        }
+        return -1;
+    }
+}
