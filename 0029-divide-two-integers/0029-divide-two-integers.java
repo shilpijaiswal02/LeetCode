10c0691355c76aa252b43347664ccc47230b0686
Shilpi Kumari <shilpijais9693@gmail.com>
2026-07-17 14:02:40 +0530

    Time: 1 ms (93.02%), Space: 42.4 MB (84.38%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+       if(dividend==Integer.MIN_VALUE && divisor==-1){
+        return Integer.MAX_VALUE;
+       }
+        int res=dividend/divisor;
+           
+        return res;
+    }
+}
