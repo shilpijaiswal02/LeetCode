bc22fedc1622c6d905da11aa35f70172015e0ac6
Shilpi Kumari <shilpijais9693@gmail.com>
2026-07-28 22:47:55 +0530

    Time: 33 ms (43.78%), Space: 48.3 MB (38.15%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,5 +1,47 @@
 class Solution {
     public String smallestPalindrome(String s) {
+
+        int[] freq = new int[26];
+
+        // Count frequency of each character
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        StringBuilder left = new StringBuilder();
+        char middle = 0;
+
+        // Build the left half and find the middle character
+        for (int i = 0; i < 26; i++) {
+
+            for (int j = 0; j < freq[i] / 2; j++) {
+                left.append((char) ('a' + i));
+            }
+
+            if (freq[i] % 2 == 1) {
+                middle = (char) ('a' + i);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        ans.append(left);
+
+        if (middle != 0) {
+            ans.append(middle);
+        }
+
+        ans.append(new StringBuilder(left).reverse());
+
+        return ans.toString();
+    }
+}
+
+
+/*
+
+class Solution {
+    public String smallestPalindrome(String s) {
         if(s.length()==1)return s;
         int len=s.length();
         StringBuilder sb=new StringBuilder();
@@ -42,4 +84,4 @@ class Solution {
 
         return sb.toString();
     }
-}
+}*/
