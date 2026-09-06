3fc189ee72f416257b781467b0397b763e90c7d8
Shilpi Kumari <shilpijais9693@gmail.com>
2026-09-07 01:33:04 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,23 @@
 class Solution {
     public int fib(int n) {
-        if(n==0){
+          if(n==0){
             return 0;
         }
+        int first=0;
+        int second=1;
+        for(int i=2;i<=n;i++){
+          int third=first+second;
+          first=second;
+          second=third;
+        }
+
+        return second;
+        /*
         if(n==1){
             return 1;
 
         }
 
-        return fib(n-1)+fib(n-2);
+        return fib(n-1)+fib(n-2);*/
     }
 }
